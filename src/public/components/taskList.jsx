bcbc4762e0a024ea1116ee184a1f118fa2d07193
ZIVR <zivros@gmail.com>
2015-07-21 16:23:09 +0300

    better UI

--- src/public/components/taskList.jsx
+++ src/public/components/taskList.jsx
@@ -4,7 +4,10 @@ export class taskList extends React.Component {
     constructor(){
         super();
         this.state = {
-            tasks:["task1","task2","task3","task4"]
+            tasks:[{name: "task1", status:"init"},
+                {name: "task2", status:"started"},
+                {name: "task3", status:"finished"},
+                {name: "task4", status:"error"}]
         }
         eventsDispatcher.pathStore.listen((tasks) => {
             this.state.tasks = tasks;
@@ -20,13 +23,14 @@ export class taskList extends React.Component {
             { 
               this.state.tasks.map((item) => {
                    var boundClick = this.handleClick.bind(this, item);
-                   return <section className="section--center mdl-grid mdl-grid--no-spacing mdl-shadow--2dp">
+                  var classname = "section--center mdl-grid mdl-grid--no-spacing mdl-shadow--2dp task-status-" + item.status;
+                   return <section className={classname} >
                        <header className="section__play-btn mdl-cell mdl-button mdl-cell--3-col-desktop mdl-cell--2-col-tablet mdl-cell--4-col-phone mdl-color--teal-100 mdl-color-text--white">
                            <i onClick={boundClick} className="material-icons">add</i>
                        </header>
                        <div className="mdl-card mdl-cell mdl-cell--9-col-desktop mdl-cell--6-col-tablet mdl-cell--4-col-phone">
                            <div className="mdl-card__supporting-text">
-                               <h4>{item}</h4>
+                               <h4>{item.name}</h4>
                            </div>
                        </div>
                    </section>
