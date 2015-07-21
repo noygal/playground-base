f59c8a07b5d5e58ed28121841c4a296695aa45d5
ZIVR <zivros@gmail.com>
2015-07-21 15:57:52 +0300

    better UI

--- src/public/components/taskList.jsx
+++ src/public/components/taskList.jsx
@@ -1,5 +1,3 @@
-
-
 var React = require('react');
 var eventsDispatcher = require('../modules/events/eventsDispatcher.js');
 export class taskList extends React.Component {
@@ -23,7 +21,7 @@ export class taskList extends React.Component {
               this.state.tasks.map((item) => {
                    var boundClick = this.handleClick.bind(this, item);
                    return <section className="section--center mdl-grid mdl-grid--no-spacing mdl-shadow--2dp">
-                       <header className="section__play-btn mdl-cell mdl-cell--3-col-desktop mdl-cell--2-col-tablet mdl-cell--4-col-phone mdl-color--teal-100 mdl-color-text--white">
+                       <header className="section__play-btn mdl-cell mdl-button mdl-cell--3-col-desktop mdl-cell--2-col-tablet mdl-cell--4-col-phone mdl-color--teal-100 mdl-color-text--white">
                            <i onClick={boundClick} className="material-icons">add</i>
                        </header>
                        <div className="mdl-card mdl-cell mdl-cell--9-col-desktop mdl-cell--6-col-tablet mdl-cell--4-col-phone">
