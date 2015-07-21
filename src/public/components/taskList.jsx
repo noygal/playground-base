810ace66a673dab747f586070e9ca5a72902873b
mwerbner <verbmat@gmail.com>
2015-07-21 15:53:49 +0300

    click on tasks

--- src/public/components/taskList.jsx
+++ src/public/components/taskList.jsx
@@ -8,21 +8,23 @@ export class taskList extends React.Component {
         this.state = {
             tasks:["task1","task2","task3","task4"]
         }
-        eventsDispatcher.pathStore.listen(function(tasks){
+        eventsDispatcher.pathStore.listen((tasks) => {
             this.state.tasks = tasks;
         })
     }
-    handleClcik(){
+    handleClick(item){
+        debugger;
         alert(item);
     }
     render() {
         return (
             <div className="commentBox">
-            { this.state.tasks.map(function(item) {
-
+            { 
+              this.state.tasks.map((item) => {
+                   var boundClick = this.handleClick.bind(this, item);
                    return <section className="section--center mdl-grid mdl-grid--no-spacing mdl-shadow--2dp">
                        <header className="section__play-btn mdl-cell mdl-cell--3-col-desktop mdl-cell--2-col-tablet mdl-cell--4-col-phone mdl-color--teal-100 mdl-color-text--white">
-                           <i className="material-icons">add</i>
+                           <i onClick={boundClick} className="material-icons">add</i>
                        </header>
                        <div className="mdl-card mdl-cell mdl-cell--9-col-desktop mdl-cell--6-col-tablet mdl-cell--4-col-phone">
                            <div className="mdl-card__supporting-text">
