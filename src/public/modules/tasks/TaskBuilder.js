3f01ac042a8ccb24fa29f95e8286948bed4fd511
Gal Noy <gal@galnoy.com>
2015-07-21 18:44:32 +0300

    Added gulp support

--- src/public/modules/tasks/TaskBuilder.js
+++ src/public/modules/tasks/TaskBuilder.js
@@ -1,12 +1,15 @@
 import _npmParser from './NpmParser'
+import _gulpParser from './GulpParser'
 
 export default class TaskBuilder {
   constructor() {
     this.npmParser = new _npmParser();
+    this.gulpParser = new _gulpParser();
   }
   processPath(projectPath) {
     var results = {
-      npm : this.npmParser.parsePath(projectPath)
+      npm : this.npmParser.parsePath(projectPath),
+      gulp : this.gulpParser.parsePath(projectPath)
     }
     return results
   }
